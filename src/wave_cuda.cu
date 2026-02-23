d5f575aafa6a77bb370b45621295162d171a4331
happikin <yashesviraina@outlook.com>
2026-02-23 19:36:28 +0000

    CUDA version implemented with CudaImplementationData usage

--- src/wave_cuda.cu
+++ src/wave_cuda.cu
@@ -81,12 +81,74 @@ void step_kernel(
 // object below, in `from_cpu_sim`.
 struct CudaImplementationData {
     // Add any data members you need here, such as device arrays, streams, etc
+    double* d_buf[3];
+    double* d_cs2;
+    double* d_damp;
+    
+    std::size_t m_nx;
+    std::size_t m_ny;
+    std::size_t m_nz;
 
-    CudaImplementationData() {
+    const WaveSimulation& m_wave_cuda;
+    size_t m_total_size;
+    size_t m_interior_size;
+    double m_factor;
+    double m_d2;
+    double m_dt;
+
+    CudaImplementationData(
+        WaveSimulation& _wave_cuda
+    ) : m_wave_cuda(_wave_cuda) {
         nvtx3::scoped_range r{"initialise"};
         // allocate them here?
+        auto [nx, ny, nz] = _wave_cuda.params.shape;
+        m_nx = nx; m_ny = ny; m_nz = nz;
+
+        m_interior_size = nx * ny * nz;
+
+        size_t nx_tot = nx + 2;
+        size_t ny_tot = ny + 2;
+        size_t nz_tot = nz + 2;
+
+        m_total_size = nx_tot * ny_tot * nz_tot;
+
+        m_d2 = _wave_cuda.params.dx * _wave_cuda.params.dx;
+        m_dt = _wave_cuda.params.dt;
+        m_factor = (m_dt * m_dt) / m_d2;
+
+        cudaMalloc(&d_buf[0], m_total_size * sizeof(double));
+        cudaMalloc(&d_buf[1], m_total_size * sizeof(double));
+        cudaMalloc(&d_buf[2], m_total_size * sizeof(double));
+
+        cudaMalloc(&d_cs2,  m_interior_size * sizeof(double));
+        cudaMalloc(&d_damp, m_interior_size * sizeof(double));
+
+    }
+
+    void copy_into() {
+        cudaMemcpy(d_buf[0], m_wave_cuda.u.now().data(),  m_total_size * sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_buf[1], m_wave_cuda.u.prev().data(), m_total_size * sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_buf[2], m_wave_cuda.u.next().data(), m_total_size * sizeof(double), cudaMemcpyHostToDevice);
+
+        cudaMemcpy(d_cs2,  m_wave_cuda.cs2.data(),  m_interior_size * sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_damp, m_wave_cuda.damp.data(), m_interior_size * sizeof(double), cudaMemcpyHostToDevice);
+
+    }
+
+    void copy_back() {
+        // cudaMemcpy(u.now().data(),  d_buf[cur],  total_size*sizeof(double), cudaMemcpyDeviceToHost);
+        // cudaMemcpy(u.prev().data(), d_buf[prev], total_size*sizeof(double), cudaMemcpyDeviceToHost);
+        // cudaMemcpy(u.next().data(), d_buf[next], total_size*sizeof(double), cudaMemcpyDeviceToHost);
+        
     }
     ~CudaImplementationData() {
+
+        // ---- Free memory ---- //
+        cudaFree(d_buf[0]);
+        cudaFree(d_buf[1]);
+        cudaFree(d_buf[2]);
+        cudaFree(d_cs2);
+        cudaFree(d_damp);
         // free here probably
    }
 };
@@ -115,7 +177,8 @@ CudaWaveSimulation CudaWaveSimulation::from_cpu_sim(const fs::path& cp, const Wa
     ans.append_u_fields();
 
     // Perhaps you want to do some device set up now?
-    // ans.impl = std::make_unique<CudaImplementationData>();
+    ans.impl = std::make_unique<CudaImplementationData>(ans);
+    // ans.impl->copy_into();
 
     return ans;
 }
@@ -162,39 +225,41 @@ void CudaWaveSimulation::append_u_fields() {
 
 void CudaWaveSimulation::run(int n)
 {
-    auto [nx, ny, nz] = params.shape;
+    // auto [nx, ny, nz] = params.shape;
 
-    size_t interior_size = nx * ny * nz;
+    // size_t interior_size = nx * ny * nz;
 
-    size_t nx_tot = nx + 2;
-    size_t ny_tot = ny + 2;
-    size_t nz_tot = nz + 2;
+    // size_t nx_tot = nx + 2;
+    // size_t ny_tot = ny + 2;
+    // size_t nz_tot = nz + 2;
 
-    size_t total_size = nx_tot * ny_tot * nz_tot;
+    // size_t total_size = nx_tot * ny_tot * nz_tot;
 
-    double d2 = params.dx * params.dx;
-    double dt = params.dt;
-    double factor = (dt * dt) / d2;
+    // double d2 = params.dx * params.dx;
+    // double dt = params.dt;
+    // double factor = (dt * dt) / d2;
 
-    // ---- Allocate fixed device buffers ---- //
-    double* d_buf[3];
-    cudaMalloc(&d_buf[0], total_size * sizeof(double));
-    cudaMalloc(&d_buf[1], total_size * sizeof(double));
-    cudaMalloc(&d_buf[2], total_size * sizeof(double));
+    // // ---- Allocate fixed device buffers ---- //
+    // double* d_buf[3];
+    // cudaMalloc(&d_buf[0], total_size * sizeof(double));
+    // cudaMalloc(&d_buf[1], total_size * sizeof(double));
+    // cudaMalloc(&d_buf[2], total_size * sizeof(double));
 
-    double* d_cs2;
-    double* d_damp;
+    // double* d_cs2;
+    // double* d_damp;
 
-    cudaMalloc(&d_cs2, interior_size * sizeof(double));
-    cudaMalloc(&d_damp, interior_size * sizeof(double));
+    // cudaMalloc(&d_cs2, interior_size * sizeof(double));
+    // cudaMalloc(&d_damp, interior_size * sizeof(double));
 
-    // ---- Copy initial data ---- //
-    cudaMemcpy(d_buf[0], u.now().data(),  total_size * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_buf[1], u.prev().data(), total_size * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_buf[2], u.next().data(), total_size * sizeof(double), cudaMemcpyHostToDevice);
+    // // ---- Copy initial data ---- //
+    // cudaMemcpy(d_buf[0], u.now().data(),  total_size * sizeof(double), cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_buf[1], u.prev().data(), total_size * sizeof(double), cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_buf[2], u.next().data(), total_size * sizeof(double), cudaMemcpyHostToDevice);
 
-    cudaMemcpy(d_cs2,  cs2.data(),  interior_size * sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_damp, damp.data(), interior_size * sizeof(double), cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_cs2,  cs2.data(),  interior_size * sizeof(double), cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_damp, damp.data(), interior_size * sizeof(double), cudaMemcpyHostToDevice);
+
+    impl->copy_into();
 
     // ---- Role indices ---- //
     int cur  = 0;
@@ -202,23 +267,23 @@ void CudaWaveSimulation::run(int n)
     int next = 2;
 
     int threads = 256;
-    int blocks = (interior_size + threads - 1) / threads;
+    int blocks = (impl->m_interior_size + threads - 1) / threads;
 
     for (int t = 0; t < n; ++t)
     {
         step_kernel<<<blocks, threads>>>(
-            d_buf[cur],
-            d_buf[prev],
-            d_buf[next],
-            d_cs2,
-            d_damp,
-            nx,
-            ny,
-            nz,
-            ny_tot,
-            nz_tot,
-            factor,
-            dt
+            impl->d_buf[cur],
+            impl->d_buf[prev],
+            impl->d_buf[next],
+            impl->d_cs2,
+            impl->d_damp,
+            impl->m_nx,
+            impl->m_ny,
+            impl->m_nz,
+            impl->m_ny + 2,
+            impl->m_nz + 2,
+            impl->m_factor,
+            impl->m_dt
         );
 
         cudaDeviceSynchronize();
@@ -235,19 +300,20 @@ void CudaWaveSimulation::run(int n)
 
     // Copy final result back (cur matches u.now()) //
     cudaMemcpy(u.now().data(),
-               d_buf[cur],
-               total_size * sizeof(double),
+               impl->d_buf[cur],
+               impl->m_total_size * sizeof(double),
                cudaMemcpyDeviceToHost);
 
     // ---- copy back all three buffers to make them persist between run calls ---- //
-    cudaMemcpy(u.now().data(),  d_buf[cur],  total_size*sizeof(double), cudaMemcpyDeviceToHost);
-    cudaMemcpy(u.prev().data(), d_buf[prev], total_size*sizeof(double), cudaMemcpyDeviceToHost);
-    cudaMemcpy(u.next().data(), d_buf[next], total_size*sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(u.now().data(),  impl->d_buf[cur],  impl->m_total_size*sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(u.prev().data(), impl->d_buf[prev], impl->m_total_size*sizeof(double), cudaMemcpyDeviceToHost);
+    cudaMemcpy(u.next().data(), impl->d_buf[next], impl->m_total_size*sizeof(double), cudaMemcpyDeviceToHost);
     
     // ---- Free memory ---- //
-    cudaFree(d_buf[0]);
-    cudaFree(d_buf[1]);
-    cudaFree(d_buf[2]);
-    cudaFree(d_cs2);
-    cudaFree(d_damp);
+    // cudaFree(d_buf[0]);
+    // cudaFree(d_buf[1]);
+    // cudaFree(d_buf[2]);
+    // cudaFree(d_cs2);
+    // cudaFree(d_damp);
+    // --------------------- //
 }
